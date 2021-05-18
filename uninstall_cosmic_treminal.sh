7153f745a05e0286b21e385d1eeeef441f444b87
Mahbub Zaman <mahbubzaman17@gmail.com>
2021-05-18 20:26:26 +1000

    Update uninstall_cosmic_treminal.sh

--- uninstall_cosmic_treminal.sh
+++ uninstall_cosmic_treminal.sh
@@ -17,11 +17,10 @@ confirm "Are you sure you want to uninstall oh-my-zsh?" && sh -c "$(curl -fsSL h
 
 confirm "Are you sure you want to uninstall itemr2?" && brew cask uninstall iterm2
 
+# Need to check this
 confirm "Are you sure you want to uninstall brew?" && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
 
 confirm "Are you sure you want to remove Droid Sans Mono for Powerline Nerd Font Complete.otf?" && rm ~/Library/Fonts/Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete.otf
 
 
 chsh -s /bin/bash
-
-
