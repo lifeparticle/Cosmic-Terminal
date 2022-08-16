a4f4f8bb87758513fd8a2f361c4e0262cf10444b
Mahbub Zaman <mahbubzaman17@gmail.com>
2022-08-16 22:21:22 +1000

    Update install_cosmic_terminal.sh

--- install_cosmic_terminal.sh
+++ install_cosmic_terminal.sh
@@ -73,8 +73,5 @@ fi
 
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
-# chk this
-chsh -s /bin/zsh
-
 install "fig" && brew install fig
 fig
